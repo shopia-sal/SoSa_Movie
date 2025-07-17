c9bf718cdeb2a727c72944c8d5cfc9521689fdf0
Shopia Salma🩷 <raniya.niloufar.sopayana@wbi.ac.id>
2025-07-17 15:48:59 +0700

    fix: downgrade React to 18.2.0 and fix build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 import { OutlineButton } from '../component/button/Button';
